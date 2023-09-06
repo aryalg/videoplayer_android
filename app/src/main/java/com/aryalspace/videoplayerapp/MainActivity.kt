760f0cc39a9093ed7d3498b7d50110ff401df35a
Bikram Aryal <bikram.aryall@gmail.com>
2023-09-06 17:40:45 +0545

    Displaying Videos from Local Storage

---
+++ app/src/main/java/com/aryalspace/videoplayerapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.aryalspace.videoplayerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Displaying videos from local storage
+        val videoView: VideoView = findViewById(R.id.videoview1)
+        videoView.setVideoPath(
+            "android.resource://"
+                    +packageName+"/"
+                    +R.raw.mountains
+        )
+
+
+        var mediaController =  MediaController(this)
+
+
+        mediaController.setAnchorView(videoView)
+
+        mediaController.setMediaPlayer(videoView)
+
+        videoView.setMediaController(mediaController)
+
+    videoView.start()
+
+
+    }
+}
